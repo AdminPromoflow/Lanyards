c41a9abe9b58949d078cb00230bc89123181461a
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2024-09-14 06:38:36 -0500

    ...

---
+++ views/global/customize-lanyard/sections2/Templates/one-side-15mm.php
@@ -0,0 +1,55 @@
+<!-- Main container for the lanyard preview section with ID "one-side-15mm" and class "one-side-25mm" -->
+<div id="one-side-15mm" class="one-side-25mm">
+
+  <!-- Inner container holding the lanyard design -->
+  <div class="container-one-side-25mm">
+
+    <!-- Section to draw the lanyard -->
+    <div class="draw-lanyard">
+
+      <!-- Standard lanyard design (normal) for 15mm one-sided lanyard -->
+      <div class="draw-lanyard-normal standar">
+
+        <!-- Back clip section for the 15mm lanyard -->
+        <div class="back-clip-os-15mm">
+          <img src="../../views/assets/img/global/customize-lanyard/sections2/templates/clip-neck-1.png" alt="">
+          <img src="../../views/assets/img/global/customize-lanyard/sections2/templates/clip-neck-2.png" alt="">
+        </div>
+
+        <!-- Left side of the lanyard with background color -->
+        <div class="os-normal-left-15mm backgroundColour">
+          <div class="draw-os-normal-left-15mm text-container draw-os-15mm"></div>
+        </div>
+
+        <!-- Right side of the lanyard with background color -->
+        <div class="os-normal-right-15mm backgroundColour">
+          <div class="draw-os-normal-right-15mm text-container draw-os-15mm"></div>
+        </div>
+
+      </div>
+
+      <?php include "../../views/global/customize-lanyard/sections2/Templates/one-side-with-attachment-15mm.php" ?>
+
+
+    </div>
+
+    <!-- Clip section outside of the lanyard design -->
+    <div class="os25-clip img-clip os25-clip-15mm">
+      <img class="" src="../../views/assets/img/global/customize-lanyard/sections2/clips/25-one-end-clip.png" alt="">
+    </div>
+  </div>
+
+  <!-- Container for two-sided printed lanyard -->
+  <div class="container-one-side-25mm twoSidePrinted">
+    <div class="os25-img">
+      <img class="standar" src="../../views/assets/img/global/customize-lanyard/sections2/templates/15-one-end.png" alt="">
+      <img class="attachment" src="../../views/assets/img/global/customize-lanyard/sections2/templates/15-one-end-attachment.png" alt="">
+    </div>
+    <div class="attachmentThing os-attachment-15">
+      <img src="../../views/assets/img/global/customize-lanyard/sections/attachment/quick-release-black.png" alt="">
+    </div>
+    <div class="os25-clip img-clip os25-clip-15mm">
+      <img class="" src="../../views/assets/img/global/customize-lanyard/sections2/clips/25-one-end-clip.png" alt="">
+    </div>
+  </div>
+</div>
---
+++ views/global/customize-lanyard/sections2/Templates/one-side-25mm.php
@@ -0,0 +1,59 @@
+<!-- Main container for the lanyard preview section with ID "one-side-25mm" and class "one-side-25mm" -->
+<div id="one-side-25mm" class="one-side-25mm">
+
+  <!-- Inner container holding the lanyard design -->
+  <div class="container-one-side-25mm">
+
+    <!-- Section to draw the lanyard -->
+    <div class="draw-lanyard">
+
+      <!-- Standard lanyard design (normal) for 25mm one-sided lanyard -->
+      <div class="draw-lanyard-normal standar">
+
+        <!-- Back clip section for the 25mm lanyard -->
+        <div class="back-clip-one-end-25mm">
+          <img src="../../views/assets/img/global/customize-lanyard/sections2/templates/clip-neck-1.png" alt="">
+          <img src="../../views/assets/img/global/customize-lanyard/sections2/templates/clip-neck-2.png" alt="">
+        </div>
+
+        <!-- Left side of the lanyard with background color -->
+        <div class="one-end-normal-left-25mm backgroundColour">
+          <div class="draw-os-normal-left-25mm text-container draw-os-25mm"></div>
+        </div>
+
+        <!-- Right side of the lanyard with background color -->
+        <div class="one-end-normal-right-25mm backgroundColour">
+          <div class="draw-os-normal-right-25mm text-container draw-os-25mm"></div>
+        </div>
+
+      </div>
+
+      <?php include "../../views/global/customize-lanyard/sections2/Templates/one-side-with-attachment-25mm.php" ?>
+
+
+    </div>
+
+    <!-- Clip section outside of the lanyard design -->
+    <div class="attachmentThing os-attachment-25">
+      <img src="../../views/assets/img/global/customize-lanyard/sections/attachment/quick-release-black.png" alt="">
+      <img class="attachment" src="../../views/assets/img/global/customize-lanyard/sections2/templates/25-one-end-attachment.png" alt="">
+    </div>
+    <div class="os25-clip img-clip os25-clip-25mm">
+      <img class="" src="../../views/assets/img/global/customize-lanyard/sections2/clips/25-one-end-clip.png" alt="">
+    </div>
+  </div>
+
+  <!-- Container for two-sided printed lanyard -->
+  <div class="container-one-side-25mm twoSidePrinted">
+    <div class="os25-img">
+      <img class="standar" src="../../views/assets/img/global/customize-lanyard/sections2/templates/25-one-end.png" alt="">
+      <img class="attachment" src="../../views/assets/img/global/customize-lanyard/sections2/templates/25-one-end-attachment.png" alt="">
+    </div>
+    <div class="attachmentThing os-attachment-25">
+      <img src="../../views/assets/img/global/customize-lanyard/sections/attachment/quick-release-black.png" alt="">
+    </div>
+    <div class="os25-clip img-clip os25-clip-25mm">
+      <img class="" src="../../views/assets/img/global/customize-lanyard/sections2/clips/25-one-end-clip.png" alt="">
+    </div>
+  </div>
+</div>
