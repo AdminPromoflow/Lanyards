d08cb61bae30f8e7aa799ba7d48f86d829d5064b
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2024-10-03 05:08:25 -0500

    “…”

--- controller/users/register.php
+++ controller/users/register.php
@@ -107,11 +107,12 @@ class ApiHandler {
 require_once '../../models/users.php';
 
 require_once '../../controller/users/send-emails.php';
-echo "string";exit;
+require_once '../config/database.php';
+
+echo "string2";exit;
 
 require_once '../config/security.php';
 
-require_once '../config/database.php';
 
 
 // Create an instance of the ApiHandler class and handle the request
